1a32759978fa7d52c71db06d5c000a1a6afef28d
Даниил <KostromDan@gmail.com>
2023-08-19 19:44:22 +0300

    Fix errors while parsing pastebin if download count is more than 1000 beacuse of ",".

--- common/src/main/java/dev/kostromdan/mods/netjs/tasks/subtasks/PasteBinMetaDataTask.java
+++ common/src/main/java/dev/kostromdan/mods/netjs/tasks/subtasks/PasteBinMetaDataTask.java
@@ -2,6 +2,7 @@ package dev.kostromdan.mods.netjs.tasks.subtasks;
 
 import dev.kostromdan.mods.netjs.tasks.AbstractNetJSTask;
 import dev.kostromdan.mods.netjs.utils.NetJSUtils;
+import dev.latvian.mods.kubejs.util.ConsoleJS;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -48,53 +49,59 @@ public class PasteBinMetaDataTask extends AbstractNetJSTask {
 			return;
 		}
 
+		try {
+			Element post_name = elems.getElementsByClass("info-top").first();
+			result.put("post_name", post_name != null ? post_name.text() : null);
+			Element author_username = elems.getElementsByClass("username").first();
+			result.put("author_username", author_username != null ? author_username.text() : null);
 
-		Element post_name = elems.getElementsByClass("info-top").first();
-		result.put("post_name", post_name != null ? post_name.text() : null);
-		Element author_username = elems.getElementsByClass("username").first();
-		result.put("author_username", author_username != null ? author_username.text() : null);
-
-		Element date = elems.getElementsByClass("date").first();
-		boolean is_edited = false;
-		if (date != null) {
-			Elements date_elems = date.select("span[title]");
-			if (date_elems.size() >= 1) {
-				Element date_created = date_elems.first();
-				result.put("date_created", date_created != null ? date_created.attr("title") : null);
+			Element date = elems.getElementsByClass("date").first();
+			boolean is_edited = false;
+			if (date != null) {
+				Elements date_elems = date.select("span[title]");
+				if (date_elems.size() >= 1) {
+					Element date_created = date_elems.first();
+					result.put("date_created", date_created != null ? date_created.attr("title") : null);
+				}
+				if (date_elems.size() >= 2) {
+					is_edited = true;
+					Element date_edited = date.select("span[title]").get(1);
+					result.put("date_edited", date_edited != null ? date_edited.attr("title").replace("Last edit on: ", "") : null);
+				}
 			}
-			if (date_elems.size() >= 2) {
-				is_edited = true;
-				Element date_edited = date.select("span[title]").get(1);
-				result.put("date_edited", date_edited != null ? date_edited.attr("title").replace("Last edit on: ", "") : null);
+			result.put("is_edited", is_edited);
+			Element expire = elems.getElementsByClass("expire").first();
+			result.put("date_expire", expire != null ? expire.text() : null);
+
+			Element visits = elems.getElementsByClass("visits").first();
+			result.put("visits_count", visits != null ? Integer.parseInt(visits.text().replaceAll(",| ", "")) : null);
+			Element stars = elems.getElementsByClass("rating js-post-rating").first();
+			result.put("stars_count", stars != null ? Integer.parseInt(stars.text().replaceAll(",| ", "")) : null);
+
+			Element left = elems.getElementsByClass("left").first();
+			Elements left_elems;
+			String lang = null;
+			String category = null;
+			String size = null;
+			if (left != null && (left_elems = left.children()).size() >= 2) {
+				lang = left_elems.get(0).text();
+				category = left_elems.get(1).text().replace("| ", "");
+				size = left.text().replace(lang, "").split("\\|")[0].strip();
 			}
+			result.put("lang", lang);
+			result.put("category", category);
+			result.put("size", size);
+
+			Element likes = elems.getElementsByClass("btn -small -like").first();
+			result.put("likes_count", likes != null ? Integer.parseInt(likes.text().replaceAll(",| ", "")) : null);
+			Element dislikes = elems.getElementsByClass("btn -small -dislike").first();
+			result.put("dislikes_count", dislikes != null ? Integer.parseInt(dislikes.text().replaceAll(",| ", "")) : null);
+		}catch (Exception e){
+			ConsoleJS.SERVER.log("Error while parsing additional prams from pastebin:");
+			e.printStackTrace();
+			exception(e);
+			return;
 		}
-		result.put("is_edited", is_edited);
-		Element expire = elems.getElementsByClass("expire").first();
-		result.put("date_expire", expire != null ? expire.text() : null);
-
-		Element visits = elems.getElementsByClass("visits").first();
-		result.put("visits_count", visits != null ? Integer.parseInt(visits.text()) : null);
-		Element stars = elems.getElementsByClass("rating js-post-rating").first();
-		result.put("stars_count", stars != null ? Integer.parseInt(stars.text()) : null);
-
-		Element left = elems.getElementsByClass("left").first();
-		Elements left_elems;
-		String lang = null;
-		String category = null;
-		String size = null;
-		if (left != null && (left_elems = left.children()).size() >= 2) {
-			lang = left_elems.get(0).text();
-			category = left_elems.get(1).text().replace("| ", "");
-			size = left.text().replace(lang, "").split("\\|")[0].strip();
-		}
-		result.put("lang", lang);
-		result.put("category", category);
-		result.put("size", size);
-
-		Element likes = elems.getElementsByClass("btn -small -like").first();
-		result.put("likes_count", likes != null ? Integer.parseInt(likes.text()) : null);
-		Element dislikes = elems.getElementsByClass("btn -small -dislike").first();
-		result.put("dislikes_count", dislikes != null ? Integer.parseInt(dislikes.text()) : null);
 
 		success();
 	}
